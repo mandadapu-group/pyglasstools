5b0233015bf774682caab4341e2ace30e9b0eb54
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2020-03-24 19:33:12 -0700

    Added Coarse Grain function class & modules

--- pyglasstools/__init__.py
+++ pyglasstools/__init__.py
@@ -3,4 +3,5 @@
 from pyglasstools import _pyglasstools;
 from pyglasstools import utils;
 from pyglasstools import potential;
+from pyglasstools import cgfunc;
 
--- pyglasstools/module-pyglasstools.cpp
+++ pyglasstools/module-pyglasstools.cpp
@@ -1,10 +1,16 @@
 #include "SimBox.h"
 #include "PairPotential.h"
+#include "Quadrature.h"
+#include "CoarseGrainFunction.h"
 
 #include "extern/pybind11/include/pybind11/pybind11.h"
 
+//Typedefs several pair potentials here
 typedef PairPotential<LennardJones> PairPotentialLJ;
 typedef PairPotential<ForceShiftedLennardJones> PairPotentialForceShiftedLJ;
+
+//Typedefs several coarsegrain functions  here
+typedef CoarseGrainFunction<Octic> OcticFunc;
 //! Create the python module
 /*! each class setup their own python exports in a function export_ClassName
  create the pyglasstools python module and define the exports here.
@@ -15,4 +21,6 @@ PYBIND11_MODULE(_pyglasstools, m)
     export_SimBox(m);
     export_PairPotential<PairPotentialLJ>(m, "PairPotentialLJ");
     export_PairPotential<PairPotentialForceShiftedLJ>(m, "PairPotentialForceShiftedLJ");
+    
+    export_CoarseGrainFunction<OcticFunc>(m, "OcticFunc");
 }

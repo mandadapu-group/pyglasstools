850f4163b3d54cf7156c98cf9f7983d6e281017e
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2020-03-25 23:33:19 -0700

    Edited CMakefiles. Boost and Aboria is now a dependency

--- CompilerFlagsSetup.cmake
+++ CompilerFlagsSetup.cmake
@@ -1,13 +1,13 @@
 # enable C++11
 if (CMAKE_VERSION VERSION_GREATER 3.0.99)
     # let cmake set the flags
-    set(CMAKE_CXX_STANDARD 11)
+    set(CMAKE_CXX_STANDARD 14)
     set(CMAKE_CXX_STANDARD_REQUIRED ON)
 else()
     # manually enable flags
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
 endif()
 
 # Enable compiler warnings and O2 optimization on gcc and g++
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -Wall -Wno-unknown-pragmas -Wno-deprecated-declarations")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2 -Wall -Wno-unknown-pragmas")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -march=native -Wall -Wno-unknown-pragmas -Wno-deprecated-declarations")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -march=native -Wall -Wno-unknown-pragmas")

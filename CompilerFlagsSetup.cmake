eea773e81d1885a46945f6b034c063db3605c67a
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2020-03-27 15:38:45 -0700

    Added aggresive compiler flags

--- CompilerFlagsSetup.cmake
+++ CompilerFlagsSetup.cmake
@@ -9,5 +9,7 @@ else()
 endif()
 
 # Enable compiler warnings and O2 optimization on gcc and g++
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -march=native -Wall -Wno-unknown-pragmas -Wno-deprecated-declarations")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -march=native -Wall -Wno-unknown-pragmas")
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -msse3 -march=native -fopenmp -Wall -Wno-unknown-pragmas -Wno-deprecated-declarations")
+#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -msse3 -march=native -fopenmp -Wall -Wno-unknown-pragmas")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Ofast -msse3 -march=native -Wall -Wno-unknown-pragmas -Wno-deprecated-declarations")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Ofast -msse3 -march=native -Wall -Wno-unknown-pragmas")

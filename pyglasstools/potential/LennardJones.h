d493ab21cf25162bc7c3ff7b79393decad196b14
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2020-03-28 21:40:25 -0700

    Added stiffness calculation for LJ systems

--- pyglasstools/potential/LennardJones.h
+++ pyglasstools/potential/LennardJones.h
@@ -34,6 +34,26 @@ class LennardJones
             else
                 return 0.0;
         }
+        
+        //! Evaluate the force and energy
+        virtual double computeStiffness(double d_i, double d_j)
+        {
+            double sigma = 0.5*(d_i+d_j);
+            double actualrcutsq = rcutsq*sigma*sigma;
+            
+            // compute the force divided by r in force_divr
+            if (rsq < actualrcutsq && eps != 0)
+                {
+                double r2inv = 1.0/rsq;
+                r2inv *= sigma*sigma;
+                double r6inv = r2inv * r2inv * r2inv;
+                double phi_rr = 4*eps*r2inv * r6inv * (13.0*12.0*r6inv - 6.0*7.0);
+                return phi_rr;
+                }
+            else
+                return 0.0;
+        }
+
         virtual double computeRcut(double d_i, double d_j)
         {
             double sigma = 0.5*(d_i+d_j);

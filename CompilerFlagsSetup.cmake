4581c4c647b69a241ca0d2b6defa018fe69d611d
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2020-03-30 16:45:01 -0700

    Clean up compiler flags

--- CompilerFlagsSetup.cmake
+++ CompilerFlagsSetup.cmake
@@ -8,8 +8,6 @@ else()
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
 endif()
 
-# Enable compiler warnings and O2 optimization on gcc and g++
-#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -msse3 -march=native -fopenmp -Wall -Wno-unknown-pragmas -Wno-deprecated-declarations")
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -msse3 -march=native -fopenmp -Wall -Wno-unknown-pragmas")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Ofast -msse3 -march=native -Wall -Wno-unknown-pragmas -Wno-deprecated-declarations")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Ofast -msse3 -march=native -Wall -Wno-unknown-pragmas")
+# Enable compiler warnings and O2 opeimization on gcc and g++
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Ofast -flto -march=native -Wall -Wno-unknown-pragmas -Wno-deprecated-declarations")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Ofast -flto -march=native -Wall -Wno-unknown-pragmas")

a3bf293a4eca5e96705a88b722f76c6d20a67a8e
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2020-04-23 12:56:06 -0700

    Added trajectory analysis automation for irving kirkwood fields

--- pyglasstools/__init__.py
+++ pyglasstools/__init__.py
@@ -5,9 +5,31 @@ from pyglasstools import utils;
 from pyglasstools import calculator;
 from pyglasstools import observables
 from os import path
+import numpy as np
 
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+nprocs = size
 #The module should save more than one logger, which analyzes various observables
 loggers = [];
+
+#https://stackoverflow.com/questions/45680050/cannot-write-to-shared-mpi-file-with-mpi4py
+class MPILogFile(object):
+    def __init__(self, comm, filename, mode):
+        self.file_handle = MPI.File.Open(comm, filename, mode)
+        self.file_handle.Set_atomicity(True)
+        self.buffer = bytearray
+
+    def write(self, msg):
+        b = bytearray()
+        b.extend(map(ord, msg))
+        self.file_handle.Write_shared(b)
+
+    def close(self):
+        self.file_handle.Sync()
+        self.file_handle.Close()
 
 #will automatically create the required calculators . . .
 class logger(object):
@@ -76,6 +98,121 @@ class logger(object):
         self.global_obs = observables.initialize_global(self.__glob_obs_names)
         for name in self.global_obs:
             self.ikglobal.addObservable(self.global_obs[name]._getObservable())
+
+#will automatically create the required calculators . . .
+class field_logger(object):
+    global loggers 
+
+    def __init__(self, fileprefix=None, names = None, sysdata = None, pair = None, cgfunc = None, dx=1.0):
+        
+        #Field logger a
+        if rank == 0: 
+            nmax = int(sysdata.simbox.boxsize[0]/dx)
+            points = np.linspace(-sysdata.simbox.boxsize[0]/2.0,+sysdata.simbox.boxsize[0]/2.0,nmax)
+            x = []
+            for i in range(len(points)): 
+                for j in range(len(points)):
+                    x.append(np.array([points[i],points[j],0]).astype(np.float64))
+            gridpoints = np.asarray(x,dtype=np.float64)
+            
+            # determine the size of each sub-task
+            ave, res = divmod(len(gridpoints), nprocs)
+            counts = [ave + 1 if p < res else ave for p in range(nprocs)]
+
+
+            # determine the starting and ending indices of each sub-task
+            starts = [sum(counts[:p]) for p in range(nprocs)]
+            ends = [sum(counts[:p+1]) for p in range(nprocs)]
+
+            # converts gridpoints into a list of arrays 
+            gridpoints = [gridpoints[starts[p]:ends[p]] for p in range(nprocs)]
+        else:
+            gridpoints = None
+        self.gridpoints = comm.scatter(gridpoints, root=0)
+        
+        #Delete unnecessarry objects
+        del gridpoints
+        if rank == 0:
+            del nmax,points,ave,res,counts,starts,ends
+        
+        #Save filename
+        self.fileprefix = fileprefix
+        
+        #First, parse the list of names based on what type of obsercables they are
+        self.__field_obs_names = [s for s in names if "f_" in s ];
+        self.field_obs = [] 
+        
+        #Initialize system data and pair potential of the system
+        self.sysdata = sysdata;
+        self.pair = pair;
+        self.cgfunc = cgfunc;
+        
+        #Initialize the "calculators"
+        self.ikfield = None;
+        
+        if not (not self.__field_obs_names):
+            #Construct the field calculator
+            self.ikfield = _pyglasstools.LocalCalculator(self.sysdata._getParticleSystem(),self.pair._getPairPotential(),self.cgfunc._getCGFunc())
+            
+            #Then, add observables
+            self.field_obs = observables.initialize_field(self.__field_obs_names,len(self.gridpoints))
+            for name in self.field_obs:
+                self.ikfield.addObservable(self.field_obs[name]._getObservable())
+        
+        #Once initialization is done, the logger adds itself to the field list of available loggers 
+        loggers.append(self)
+
+        #Finally, we create a list of trajectory files
+        #Initialize file to save:
+        self.file = {}
+        for name in self.__field_obs_names: 
+            self.file[name] = MPILogFile(comm, self.fileprefix+"_"+name, MPI.MODE_WRONLY | MPI.MODE_CREATE | MPI.MODE_APPEND)
+    def run(self): 
+        if not (not self.__field_obs_names):
+            self.ikfield.computelocal(self.gridpoints)
+    
+    def save(self,frame_num):
+        for name in self.__field_obs_names:
+            val = 0
+            signal = False
+            if "f_virialstress" in name:
+                val = np.array(self.field_obs['f_virialstress'].getVal())[:,int(name[-2]),int(name[-1])]
+            else:
+                continue
+
+            if rank == 0:
+                self.file[name].write("Frame {:d} \n".format(frame_num))
+                for i in range(len(self.gridpoints)):
+                    self.file[name].write("{:.12f} {:.12f} {:.12f} {:.12f} \n".format(  self.gridpoints[i][0],
+                                                                                        self.gridpoints[i][1], 
+                                                                                        self.gridpoints[i][2],
+                                                                                        val[i]))
+                signal = True
+                if size > 1:
+                    comm.send(signal, dest = rank+1)
+            else:
+                
+                while signal == False:
+                    signal = comm.recv(source = rank-1)
+                for i in range(len(self.gridpoints)):
+                    self.file[name].write("{:.12f} {:.12f} {:.12f} {:.12f} \n".format(  self.gridpoints[i][0],
+                                                                                        self.gridpoints[i][1], 
+                                                                                        self.gridpoints[i][2],
+                                                                                        val[i]))
+                
+                if rank < size-1:
+                    comm.send(signal, dest = rank+1)
+                else:
+                    self.file[name].write("\n")
+    
+    def update(self,frame_num):
+        self.sysdata.update(frame_num);
+        #Update the field calculator
+        self.ikfield = _pyglasstools.LocalCalculator(self.sysdata._getParticleSystem(),self.pair._getPairPotential(),self.cgfunc._getCGFunc())
+        #Then, add observables again
+        self.field_obs = observables.initialize_field(self.__field_obs_names,len(self.gridpoints))
+        for name in self.field_obs:
+            self.ikfield.addObservable(self.field_obs[name]._getObservable())
 
 def analyze(frame_list):
     for frame_num in frame_list:
--- pyglasstools/observables/__init__.py
+++ pyglasstools/observables/__init__.py
@@ -4,13 +4,22 @@ from pyglasstools.observables import _observables
 import pyglasstools
 import numpy as np
 
-def initialize_global(names = None):
+def initialize_global(names):
     list_obs = {}
     if any("g_virialstress" in s for s in names):
         list_obs['g_virialstress'] = virialstress(dim=2)
     elif any("g_kineticstress" in s for s in names):
         list_obs['g_kineticstress'] = virialstress(dim=2)
     return list_obs
+
+def initialize_field(names,length):
+    list_obs = {}
+    if any("f_virialstress" in s for s in names):
+        list_obs['f_virialstress'] = virialstressfield(2,length)
+    elif any("f_kineticstress" in s for s in names):
+        list_obs['f_kineticstress'] = kineticstressfield(2,length)
+    return list_obs
+
 class virialstress(object):
     def __init__(self, dim):
         self.Tv = _observables.GlobalVirialStress("g_virialstress", "2-TENSOR", False, True, dim)#rcut
@@ -41,53 +50,23 @@ class virialstressfield(object):
 
     """
     def __init__(self, dim, gridsize):
-        self.Tv = _observables.VirialStressField("Virial Stress Field", "2-TENSOR", False, True, dim,gridsize)
-    #Redefine attributes so that it directly access SimBox C++ class 
-    #attributes
-    def __getattr__(self,attr):
-            orig_attr = self.Tv.__getattribute__(attr)
-            if callable(orig_attr):
-                def hooked(*args, **kwargs):
-                    self.pre()
-                    result = orig_attr(*args, **kwargs)
-                    # prevent Tv from becoming unwrapped
-                    if result == self.Tv:
-                        return self
-                    self.post()
-                    return result
-                return hooked
-            else:
-                return orig_attr
-    
+        self.Tv = _observables.VirialStressField("f_virialstress", "2-TENSOR", False, True, dim,gridsize)
     def _getObservable(self):
         return self.Tv
+    def getVal(self):
+        return self.Tv.val
 
 class kineticstressfield(object):
     R""" Lennard-Jones pair potential.
 
     """
     def __init__(self, dim,gridsize):
-        self.Tk = _observables.KineticStressField("Kinetic Stress Field", "2-TENSOR", True, False, dim,gridsize)#rcut
-    
-    #Redefine attributes so that it directly access SimBox C++ class 
-    #attributes
-    def __getattr__(self,attr):
-            orig_attr = self.Tk.__getattribute__(attr)
-            if callable(orig_attr):
-                def hooked(*args, **kwargs):
-                    self.pre()
-                    result = orig_attr(*args, **kwargs)
-                    # prevent Tk from becoming unwrapped
-                    if result == self.Tk:
-                        return self
-                    self.post()
-                    return result
-                return hooked
-            else:
-                return orig_attr
+        self.Tk = _observables.KineticStressField("f_kineticstress", "2-TENSOR", True, False, dim,gridsize)#rcut
     
     def _getObservable(self):
         return self.Tk
+    def getVal(self):
+        return self.Tk.val
 
 class densityfield(object):
     R""" Lennard-Jones pair potential.

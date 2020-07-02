e4e50116a0e2ef7add504fd9493fad2183dd6375
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2020-07-02 13:59:02 -0700

    Hard-coded shift amount for preconditioner

--- pyglasstools/nonaffine/SLEPcNMA.h
+++ pyglasstools/nonaffine/SLEPcNMA.h
@@ -239,7 +239,7 @@ void SLEPcNMA::getAllEigenPairs(std::string package)
         ierr = PCFactorSetMatSolverType(pc,MATSOLVERPETSC);CHKERRABORT(PETSC_COMM_WORLD,ierr);
         ierr = PCFactorSetZeroPivot(pc,(PetscReal)std::numeric_limits<double>::epsilon()*m_hessian->m_manager->pinv_tol);
         ierr = PCFactorSetShiftType(pc, MAT_SHIFT_NONZERO);CHKERRABORT(PETSC_COMM_WORLD,ierr);
-        ierr = PCFactorSetShiftAmount(pc, PETSC_DECIDE);CHKERRABORT(PETSC_COMM_WORLD,ierr);
+        ierr = PCFactorSetShiftAmount(pc, 1e-10);CHKERRABORT(PETSC_COMM_WORLD,ierr);
     }
     else if (package == "slepc-mumps")
     {
